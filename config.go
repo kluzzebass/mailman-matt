e8b8fb77b92f991568a1e0a850d1799a47bd93d8
Jan Fredrik Leversund <kluzz@radical.org>
2022-07-16 05:42:06 +0200

    fetching delivery schedule works

--- config.go
+++ config.go
@@ -12,17 +12,17 @@ import (
 
 type (
 	Config struct {
-		Development bool          `env:"DEVELOPMENT,default=false"`
-		Port        int           `env:"MATT_PORT,default=3000"`
-		ApiUrl      url.URL       `env:"MATT_API_URL,default=https://www.posten.no/levering-av-post/_/component/main/1/leftRegion/1"`
-		ApiTimeout  time.Duration `env:"MATT_API_TIMEOUT,default=3s"`
-		Domain      string        `env:"MATT_DOMAIN,default=example.com"`
-		Company     string        `env:"MATT_COMPANY,default=Acme Inc."`
-		Product     string        `env:"MATT_COMPANY,default=Example Product"`
-		Summary     string        `env:"MATT_SUMMARY,default=POST"`
-		Timezone    string        `env:"MATT_TIMEZONE,default=Europe/Oslo"`
-		Name        string        `env:"MATT_NAME,default=Matt"`
-		CacheTTL    time.Duration `env:"MATT_CACHE_TTL,default=600s"`
+		DumpConfig bool          `env:"DEVELOPMENT,default=false"`
+		Port       int           `env:"MATT_PORT,default=3000"`
+		ApiUrl     url.URL       `env:"MATT_API_URL,default=https://www.posten.no/levering-av-post/_/component/main/1/leftRegion/1"`
+		ApiTimeout time.Duration `env:"MATT_API_TIMEOUT,default=3s"`
+		Domain     string        `env:"MATT_DOMAIN,default=example.com"`
+		Company    string        `env:"MATT_COMPANY,default=Acme Inc."`
+		Product    string        `env:"MATT_COMPANY,default=Example Product"`
+		Summary    string        `env:"MATT_SUMMARY,default=POST"`
+		Timezone   string        `env:"MATT_TIMEZONE,default=Europe/Oslo"`
+		Name       string        `env:"MATT_NAME,default=Matt"`
+		CacheTTL   time.Duration `env:"MATT_CACHE_TTL,default=600s"`
 	}
 )
 
@@ -30,6 +30,9 @@ func NewConfig(ctx context.Context) Config {
 	var cfg Config
 	if err := envconfig.Process(ctx, &cfg); err != nil {
 		log.Fatal(err)
+	}
+	if cfg.DumpConfig {
+		cfg.Dump()
 	}
 	return cfg
 }
--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 
 	// AutoLoad .env file
 	_ "github.com/joho/godotenv/autoload"
@@ -10,10 +15,49 @@ import (
 func main() {
 	ctx := context.Background()
 	cfg := NewConfig(ctx)
-	if cfg.Development {
-		cfg.Dump()
-	}
+	_ = cfg
 
+	fetchSchedule(ctx, cfg, 9845)
 }
 
-// func fetchSchedule(ctx)
+func fetchSchedule(ctx context.Context, cfg Config, postCode int) {
+	ctx, _ = context.WithTimeout(ctx, cfg.ApiTimeout)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, cfg.ApiUrl.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("x-requested-with", "XMLHttpRequest")
+	req.Header.Set("user-agent", "mailman-matt")
+	req.Header.Set("accept", "*/*")
+	req.Header.Set("accept-language", "*")
+	req.Header.Set("accept-encoding", "gzip, b")
+
+	v := make(url.Values)
+	v.Add("postCode", fmt.Sprint(postCode))
+	req.URL.RawQuery = v.Encode()
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Println("Request failed:", err)
+		return
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	// Print the status code if the request succeeds
+	log.Println("Status code:", res.StatusCode)
+	log.Println("Header:", res.Header)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Reading response body failed:", err)
+		return
+	}
+
+	fmt.Println("Body:", string(body))
+}
